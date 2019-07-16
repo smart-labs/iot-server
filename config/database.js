19078ea8089cb3210252e677b7d56fda18aa4eb4
joedysonbezerra <joedysonbezerra@gmail.com>
2019-07-16 13:21:33 -0300

    Restruturando a arquitetura para Orientação Objeto

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  url: process.env.DATABASE_URL || 'mongodb://localhost/alarm',
-  flags: { useNewUrlParser: 'true', useCreateIndex: 'true' },
+  url: process.env.DATABASE_URL,
+  flags: { useNewUrlParser: true, useCreateIndex: true },
 };
